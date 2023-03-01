bd0670d785ba53c5cb50af20877c90561bb54a39
chanul <99154985+Jangchanul@users.noreply.github.com>
2023-03-01 19:54:10 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    //dim
+    $(".dim").click(function(){
+        $(".js, .alert_box, .dim").hide();
+    });
+    
+    $(".lt_alert_btn").click(function(){
+        $(".js, .alert_box, .dim").hide();
+    });
+});
